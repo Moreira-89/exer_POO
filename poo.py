cf2bd29aebf843e4d158c2cd0a28f2420c5d84ed
Lucas Moreira <contatoparalucasmoreira@gmail.com>
2023-05-01 20:55:49 -0300

    Adição do Quadrado
    Vi que tem como cacular a área do quadrado também, então seguindo a mesma logica coloquei o quadrado como opção.

--- poo.py
+++ poo.py
@@ -20,11 +20,19 @@ class Circulo(FiguraGeometrica):
     def area(self):
         raio = float(input("Digite o raio do Circulo: "))
         return math.pi * (raio ** 2)
+    
+class Quadrado(FiguraGeometrica):
+    def area(self):
+        lado = float(input("Digite o lado do quadrado: "))
+        return lado ** 2
+    
 #criei tres instancias
 triangulo = Triangulo() 
 retangulo = Retangulo()
 circulo = Circulo()
+quadrado = Quadrado()
 
 print('Área do triangulo: ', triangulo.area())
 print("Área do retangulo: ", retangulo.area())
 print("Área do circulo: ", circulo.area())
+print("Área do quadrado: ",quadrado.area())
